70b6218fe7c4a7ef9fd3314c4b66984b7cf9a526
gportalanza <58798056+gportalanza@users.noreply.github.com>
2020-04-09 22:51:49 -0500

    Two EC2 spot instances in two different subnets
    Two EC2 spot instances in two different subnets within the same VPC. Each spot instance is accessible from the internet.

--- main.tf
+++ main.tf
@@ -15,9 +15,24 @@ resource "aws_internet_gateway" "gate"{
     }
 }
 
-resource "aws_subnet" "public_subnet"{
+resource "aws_subnet" "subnet-1"{
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.100.0/24"
+    availability_zone = "${var.az}"
+    //To specify that all instances launched in this subnet should be ssigned a public IP:
+    map_public_ip_on_launch = true
+
+    #depends_on = ["aws_internet_gateway.gate"]
+
+    tags = {
+        Name = "Guido_Testing"
+    }
+}
+
+#New subnet for the new EC2 spot instance
+resource "aws_subnet" "subnet-2"{
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.200.0/24"
     availability_zone = "${var.az}"
     //To specify that all instances launched in this subnet should be ssigned a public IP:
     map_public_ip_on_launch = true
@@ -39,27 +54,25 @@ resource "aws_route_table" "public_route"{
         gateway_id = "${aws_internet_gateway.gate.id}"
     }
 
-    #route para la segunda instancia
-    route {
-        /*cidr_block: Destination argument (required)*/
-        cidr_block = "0.0.0.0/0"
-        /*gateway_id: Target argument*/
-        gateway_id = "${aws_internet_gateway.gate.id}"
-    }
-
     tags = {
         Name = "Guido_Testing"
     }
 }
 
-#aws_route_table_association: Association route_table-subnet or route_table-internet gateway or rt-virtual private gw
-resource "aws_route_table_association" "route_table_assoc" {
-    subnet_id = "${aws_subnet.public_subnet.id}"
+#aws_route_table_association: Association route_table-subnet or route_table-internet gateway or 
+#rt-virtual private gw
+resource "aws_route_table_association" "route_table_assoc-1" {
+    subnet_id = "${aws_subnet.subnet-1.id}"
     route_table_id = "${aws_route_table.public_route.id}"
 }
 
-resource "aws_security_group" "aws_sg_ec2instances" {
-  name = "aws_sg_ec2instances"
+resource "aws_route_table_association" "route_table_assoc-2" {
+    subnet_id = "${aws_subnet.subnet-2.id}"
+    route_table_id = "${aws_route_table.public_route.id}"
+}
+
+resource "aws_security_group" "ec2-spot" {
+  name = "ec2-spot"
   description = "Allow inbound and outbound traffic"
   vpc_id = "${aws_vpc.main.id}"
 
@@ -74,7 +87,8 @@ resource "aws_security_group_rule" "allow_all_from_self" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    source_security_group_id = "${aws_security_group.aws_sg_ec2instances.id}"
+    source_security_group_id = "${aws_security_group.ec2-spot.id}"
+    security_group_id = "${aws_security_group.ec2-spot.id}"
 }
 
 resource "aws_security_group_rule" "aws_sg_ingress_rule" {
@@ -84,37 +98,9 @@ resource "aws_security_group_rule" "aws_sg_ingress_rule" {
     protocol = "tcp"
     cidr_blocks = "${split(",", var.cidr-ssh-allowed)}"
     #ipv6_cidr_blocks = "${split(",", var.cidr-ssh-allowed)}"
-    security_group_id = "${aws_security_group.aws_sg_ec2instances.id}"
+    security_group_id = "${aws_security_group.ec2-spot.id}"
 }
 
-#security group for the second EC2 instance
-resource "aws_security_group" "ec2-2" {
-  name = "ec2-2"
-  description = "Allow inbound and outbound traffic"
-  vpc_id = "${aws_vpc.main.id}"
-
-  tags {
-      Name = "Guido_Testing"
-  }
-}
-//To allow ingress traffic from itself (second EC2 instance)
-resource "aws_security_group_rule" "allow_all_from_self" {
-    type = "ingress"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    source_security_group_id = "${aws_security_group.ec2-2.id}"
-}
-
-resource "aws_security_group_rule" "ec2_2_ingress_rule" {
-    type = "ingress"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = "${split(",", var.cidr-ssh-2-allowed)}"
-    #ipv6_cidr_blocks = "${split(",", var.cidr-ssh-allowed)}"
-    security_group_id = "${aws_security_group.ec2-2.id}"
-}
 #DATA BLOCK, in which an aws data resource (in this case aws_ami) is read
 #A data block requests that Terraform read from a given data source (aws_ami in this case)
 #and export the result under the given local name ("ubuntu")
@@ -140,13 +126,13 @@ resource "aws_instance" "instance-1" {
     ami = "${data.aws_ami.ubuntu.id}"
     instance_type = "t2.micro"
     key_name = "${var.k8s-ssh-key}"
-    subnet_id = "${aws_subnet.public_subnet.id}"
+    subnet_id = "${aws_subnet.subnet-1.id}"
     availability_zone = "${var.az}"
     /*private_ip = "10.0.100.200"*/
-    vpc_security_group_ids = ["${aws_security_group.aws_sg_ec2instances.id}"]
+    vpc_security_group_ids = ["${aws_security_group.ec2-spot.id}"]
 
     tags = {
-        Name = "HelloWorld_East"
+        Name = "EC2 spot instance 1"
     }
 }
 
@@ -155,12 +141,12 @@ resource "aws_instance" "instance-2" {
     ami = "${data.aws_ami.ubuntu.id}"
     instance_type = "t2.micro"
     #actually it would be best if a different key name is used for this second instance
-    key_name = "${var.k8s-ssh-key}"
-    subnet_id = "${aws_subnet.public_subnet.id}"
+    key_name = "${var.ssh-key-2}"
+    subnet_id = "${aws_subnet.subnet-1.id}"
     availability_zone = "${var.az}"
-    vpc_security_group_ids = ["${aws_security_group.ec2-2.id}"]
+    vpc_security_group_ids = ["${aws_security_group.ec2-spot.id}"]
 
     tags = {
-        Name = "EC2 instance 2"
+        Name = "EC2 spot instance 2"
     }
 }
